a4d0c9d4c98b038a834eada401f9ce4891d58be9
Luiz Ribeiro <luizribeiro@gmail.com>
2023-06-25 14:16:23 -0400

    default certain values to 0.0 when not present

--- noaa2mqtt.py
+++ noaa2mqtt.py
@@ -19,8 +19,8 @@ with ftplib.FTP(FTP_SERVER) as ftp:
         lines = f.read().decode().splitlines()[1:]
         metar_data = Metar.Metar("\n".join(lines))
 
-def value(x, unit):
-    return x.value(unit) if x else None
+def value(x, unit, default=None):
+    return x.value(unit) if x else default
 
 
 # metar dates are given in UTC
@@ -32,21 +32,21 @@ payload = {
     "station": metar_data.station_id,
     "cycle": metar_data.cycle,
     "dewpoint": value(metar_data.dewpt, "C"),
-    "ice_accretion_in_1h": value(metar_data.ice_accretion_1hr, "IN"),
-    "ice_accretion_in_3h": value(metar_data.ice_accretion_3hr, "IN"),
-    "ice_accretion_in_6h": value(metar_data.ice_accretion_6hr, "IN"),
+    "ice_accretion_in_1h": value(metar_data.ice_accretion_1hr, "IN", 0.0),
+    "ice_accretion_in_3h": value(metar_data.ice_accretion_3hr, "IN", 0.0),
+    "ice_accretion_in_6h": value(metar_data.ice_accretion_6hr, "IN", 0.0),
     "max_temperature_24h": value(metar_data.max_temp_24hr, "C"),
     "max_temperature_6h": value(metar_data.max_temp_6hr, "C"),
     "min_temperature_24h": value(metar_data.min_temp_24hr, "C"),
     "min_temperature_6h": value(metar_data.min_temp_6hr, "C"),
-    "precipitation_in_1h": value(metar_data.precip_1hr, "IN"),
-    "precipitation_in_24h": value(metar_data.precip_24hr, "IN"),
-    "precipitation_in_3h": value(metar_data.precip_3hr, "IN"),
-    "precipitation_in_6h": value(metar_data.precip_6hr, "IN"),
+    "precipitation_in_1h": value(metar_data.precip_1hr, "IN", 0.0),
+    "precipitation_in_24h": value(metar_data.precip_24hr, "IN", 0.0),
+    "precipitation_in_3h": value(metar_data.precip_3hr, "IN", 0.0),
+    "precipitation_in_6h": value(metar_data.precip_6hr, "IN", 0.0),
     "present_weather": metar_data.present_weather() if metar_data.weather else None,
     "pressure_mbar": value(metar_data.press, "MB"),
     "pressure_sea_level_mb": value(metar_data.press_sea_level, "MB"),
-    "snow_depth_in": value(metar_data.snowdepth, "IN"),
+    "snow_depth_in": value(metar_data.snowdepth, "IN", 0.0),
     "temperature": value(metar_data.temp, "C"),
     "visibility_mi": value(metar_data.vis, "SM"),
     "wind_direction": metar_data.wind_dir.value(),
